8aa883f9dee41c2b9979aa2242dd2b2840e0f173
Jowatt <34350131+Jowatt@users.noreply.github.com>
2019-03-14 12:49:15 +0100

    removed unused constants

--- sopra-fs19-template-client-master/src/components/profile/Profile.js
+++ sopra-fs19-template-client-master/src/components/profile/Profile.js
@@ -17,33 +17,6 @@ const Container = styled(BaseContainer)`
   text-align: center;
 `;
 
-const FormContainer = styled.div`
-  margin-top: 2em;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  min-height: 300px;
-  justify-content: center;
-`;
-
-const Form = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  width: 60%;
-  height: 375px;
-  font-size: 16px;
-  font-weight: 300;
-  padding-left: 37px;
-  padding-right: 37px;
-  border-radius: 5px;
-  background: linear-gradient(rgb(27, 124, 186), rgb(2, 46, 101));
-  transition: opacity 0.5s ease, transform 0.5s ease;
-`;
-
-
-
-
 class Profile extends React.Component{
     constructor(props) {
         super(props);
